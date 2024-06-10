fb5dd2bdbc534842abbf675576dda1cd4ea9fe1e
zahir535 <zahirco535@gmail.com>
2024-06-10 23:54:29 +0800

    wip

--- src/firebase-admin/firebase-admin.module.ts
+++ src/firebase-admin/firebase-admin.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { FirebaseAdminService } from './firebase-admin.service';
 
 @Module({
-  providers: [FirebaseAdminService]
+  providers: [FirebaseAdminService],
+  exports: [FirebaseAdminService],
 })
 export class FirebaseAdminModule {}
--- src/firebase-admin/firebase-admin.service.ts
+++ src/firebase-admin/firebase-admin.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import * as firebaseAdmin from 'firebase-admin';
+
+export type User = any;
+
+const firebaseParams = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain,
+};
 
 @Injectable()
-export class FirebaseAdminService {}
+export class FirebaseAdminService {
+  constructor() {
+    firebaseAdmin.initializeApp({
+      credential: firebaseAdmin.credential.cert({
+        projectId: firebaseParams.project_id,
+        privateKey: firebaseParams.private_key.replace(/\\n/g, '\n'),
+        clientEmail: firebaseParams.client_email,
+      }),
+    });
+  }
+
+  async verifyToken(token: string): Promise<unknown | undefined> {
+    return firebaseAdmin
+      .auth()
+      .verifyIdToken(token)
+      .then((decodedIdToken) => {
+        return decodedIdToken;
+      })
+      .catch(() => {
+        // log error
+        throw new Error('Token verification failed');
+      });
+  }
+}
