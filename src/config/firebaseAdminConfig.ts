f4c6a1619feada7d7242741a853697e15319f9e0
zahir535 <zahirco535@gmail.com>
2024-06-24 10:47:25 +0800

    wip initialize firebase

---
+++ src/config/firebaseAdminConfig.ts
@@ -0,0 +1,5 @@
+export default () => ({
+  projectId: process.env.PROJECT_ID,
+  privateKey: process.env.PRIVATE_KEY,
+  clientEmail: process.env.CLIENT_EMAIL,
+});
--- src/firebase-admin/firebase-admin.module.ts
+++ src/firebase-admin/firebase-admin.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { FirebaseAdminService } from './firebase-admin.service';
+import { ConfigModule } from '@nestjs/config';
+import firebaseAdminConfig from '../config/firebaseAdminConfig';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [firebaseAdminConfig],
+    }),
+  ],
   providers: [FirebaseAdminService],
   exports: [FirebaseAdminService],
 })
